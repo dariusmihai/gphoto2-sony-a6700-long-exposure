1ff4f8ab9bd9ccd56259c01e94bdb1411e14d362
Darius Mihai <dariusmihai@gmail.com>
2025-03-14 13:46:37 +0200

    update watchdog to only restart if exit-code is non-zero

--- a6700_watchdog.sh
+++ a6700_watchdog.sh
@@ -9,6 +9,14 @@ while true; do
     echo "Starting the a6700 script..."
     /bin/bash $SCRIPT
 
+     # Check if the script exited with a non-zero exit code
+    if [ $? -ne 0 ]; then
+        echo "a6700.sh exited with an error. Restarting..."
+    else
+        echo "a6700.sh exited successfully. No restart needed."
+        exit 0
+    fi
+
     # If the script exits (crashes), restart it after a short delay
     echo "a6700 script crashed or exited. Restarting in 5 seconds..."
     sleep 5
